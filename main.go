30350237d2f86ac85a107f437fa1d1bd5e3a86be
“Daichi <daichiando98@gmail.com>
2023-01-05 11:40:19 +0100

    [add] implement the swagger foundation

--- main.go
+++ main.go
@@ -10,44 +10,6 @@ import (
 
 func main() {
 
-	//ctx := middlewares.Prep()
-
-	//ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	//defer cancel()
-
-	//customers, err := cli.GetCustomers(ctx, filter)
-	//if err != nil {
-	//	fmt.Println("error ----------- 4 -----------")
-	//	fmt.Println(err)
-	//}
-	//fmt.Println("continue ----------- 4 -----------")
-
-	//req := map[string]string{
-	//	"customerID": "12",
-	//	"points":     "22",
-	//}
-	//_, err = cli.AddCustomerRewardPoints(ctx, req)
-	//if err != nil {
-	//	fmt.Println("error ----------- 5 -----------")
-	//	fmt.Println(err)
-	//}
-	//fmt.Println("continue ----------- 5 -----------")
-
-	//temp := map[string]string{
-	//	"points": "22",
-	//}
-
-	// todo:  Still don't know why it fetches all the customers
-	//cs, err := client.CustomerManager.GetCustomers(ctx, temp)
-	//if err != nil {
-	//	fmt.Println("error ----------- 6 -----------")
-	//	fmt.Println(err)
-	//}
-	//fmt.Println("continue ----------- 6 -----------")
-	//for _, customer := range cs {
-	//	fmt.Println(customer.CustomerID)
-	//}
-
 	var err error
 	var db *sqlx.DB
 
--- middlewares/router.go
+++ middlewares/router.go
@@ -2,9 +2,12 @@ package middlewares
 
 import (
 	"erply/controllers"
+	"erply/docs"
 	"erply/service"
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func NewRouter(db *sqlx.DB) *gin.Engine {
@@ -19,10 +22,25 @@ func NewRouter(db *sqlx.DB) *gin.Engine {
 	// customers
 	customer := r.Group("/customer")
 	customer.POST("create", newCon.CreateCustomer) //
-	customer.GET("fetch", newCon.FetchCustomer)    // to fetch the data difference of remote and local server
+
+	// @BasePath /api/v1
+
+	// PingExample godoc
+	// @Summary ping example
+	// @Schemes
+	// @Description do ping
+	// @Tags example
+	// @Accept json
+	// @Produce json
+	// @Success 200 {string} FetchCustomer
+	// @Router /customer/fetch [get]
+	customer.GET("fetch", newCon.FetchCustomer) // to fetch the data difference of remote and local server
 	customer.GET("get", newCon.GetCustomerByCustomerID)
 	customer.PUT("update", newCon.UpdateCustomer)
 	customer.DELETE("delete", newCon.DeleteCustomer)
+
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	return r
 }
